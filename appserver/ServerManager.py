02837b213f5287441531e4ecfb1780b568de12cc
ferratier <ferratier.lucas00@gmail.com>
2026-06-07 15:43:03 +0200

    création de la classe ServerManager pour demarer le serveur et l'arréter et faire le lien avec l'interface QML

---
+++ appserver/ServerManager.py
@@ -0,0 +1,42 @@
+from PySide6.QtCore import QObject, Signal, Slot
+from http.server import HTTPServer
+from Robot import Robot
+from Battle import Battle
+import threading
+
+class ServerManager(QObject):
+
+    #signaux envoyés à l'interface QML
+    robotConnected = Signal(str)
+    robotDisconcted = Signal(str)
+    stepReceived = Signal(str,str,str,str,int) # rid, col, arm, exp, points
+    logMessage = Signal(str)
+
+    def __init__(self, port):
+        super().__init__()
+        self.port = port
+        self.robots = dict[str, Robot] = {}
+        self.battle = Battle()
+        self.server = None
+        self.thread = None
+
+    @Slot()
+    def start(self):
+        if self.server is not None:
+            return  # déjà démarré
+
+        self.server = HTTPServer(("", self.port), MyHandler)
+        self.thread = threading.Thread(target=self.server.serve_forever)
+        self.thread.daemon = True  # le thread s'arrête si l'application se ferme
+        self.thread.start()
+        self.logMessage.emit(f"Serveur démarré sur le port {self.port}")
+
+    @Slot()
+    def stop(self):
+        if self.server is None:
+            return  # déjà arrêté
+
+        self.server.shutdown()
+        self.server = None
+        self.thread = None
+        self.logMessage.emit("Serveur arrêté")
