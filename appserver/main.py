3142df9a9f23d1f670019990db7ba20a93b5cd49
ferratier <ferratier.lucas00@gmail.com>
2026-06-07 17:49:52 +0200

    modification du main et mise en place du QML

--- appserver/main.py
+++ appserver/main.py
@@ -1,5 +1,23 @@
-from server_http import *
+import sys
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtQuickControls2 import QQuickStyle
+from ServerManager import ServerManager
 
 if __name__ == "__main__":
-    server = HTTPServer(ADDRESS, MyHandler)
-    server.serve_forever()
+    QQuickStyle.setStyle("Fusion")
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+
+    server_manager = ServerManager(8080)
+    engine.rootContext().setContextProperty("server_manager", server_manager)
+
+    engine.load("appserver/qml_interface/Main.qml")
+
+    if not engine.rootObjects():
+        sys.exit(-1)
+
+    exit_code = app.exec()
+    server_manager.stop()  # arrête proprement le serveur à la fermeture
+    del engine
+    sys.exit(exit_code)
